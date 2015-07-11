852c9f93cc2b0817344389adc4af6e976cc961c5
Mike S <foreign.guy.mike@gmail.com>
2015-07-10 22:53:49 -0400

    init

---
+++ android/src/com/distraction/cm/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.distraction.cm.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.distraction.cm.CM;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CM(), config);
+	}
+}
---
+++ desktop/src/com/distraction/cm/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.distraction.cm.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.distraction.cm.CM;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CM(), config);
+	}
+}
